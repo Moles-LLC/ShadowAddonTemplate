0eb0ca299166d0a9e9593cb1e6c1ad090c568ece
0x3C50 <constantindevalt@gmail.com>
2022-05-21 23:16:57 +0200

    updat

--- src/main/java/me/shitcum/fart/command/TestCommand.java
+++ src/main/java/me/shitcum/fart/command/TestCommand.java
@@ -1,7 +1,6 @@
 package me.shitcum.fart.command;
 
 import net.shadow.client.feature.command.Command;
-import net.shadow.client.feature.command.argument.IntegerArgumentParser;
 import net.shadow.client.feature.command.argument.StreamlineArgumentParser;
 import net.shadow.client.feature.command.coloring.ArgumentType;
 import net.shadow.client.feature.command.coloring.PossibleArgument;
